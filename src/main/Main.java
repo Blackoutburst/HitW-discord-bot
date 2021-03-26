9e7331c5eb66a27642d540167b80bf009c256011
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-26 08:14:16 -0700

    Revert: Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,11 +9,11 @@ import utils.Config;
 
 public class Main {
 
-	private static final String TOKEN = "ODI0OTQ4Mzk1NDUzMzE3MTcw.YF2zEA.BNstbYRIOO5EZDfkfrLkNNnTX_I";
-	private static final String ACTIVITY = "Hole in the Woman";
+	private static final String TOKEN = "X";
+	private static final String ACTIVITY = "Hole in the Wall";
 
-	public static final String BOT_ID = "824948395453317170";
-	public static final String API = "2fa7a8b6-fef6-46cf-8585-23e74f7752c7";
+	public static final String BOT_ID = "X";
+	public static final String API = "X";
 	public static final String PREFIX = "!";
 	
 	public static void main(String[] args) throws LoginException, IOException {
