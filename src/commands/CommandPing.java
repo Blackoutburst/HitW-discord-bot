88ee9b18f9fadcc86bd4a6562c1edc6323a4d5bc
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-30 00:20:09 -0700

    Add more info to !ping

--- src/commands/CommandPing.java
+++ src/commands/CommandPing.java
@@ -1,5 +1,8 @@
 package commands;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 import core.Command;
 import core.CommandExecutable;
 
@@ -12,9 +15,20 @@ public class CommandPing extends CommandExecutable {
 	@Override
 	protected boolean execute() {
 		long time = System.currentTimeMillis();
+		Runtime rt = Runtime.getRuntime();
+		long memTotal = rt.totalMemory();
+		long memMax = rt.maxMemory();
+		long memLeft = rt.freeMemory();
+		long memUsed = memTotal - memLeft;
+		long mbTotal = memTotal / 1048576;
+		long mbMax = memMax / 1048576;
+		double mbUsed = (double) memUsed / 1048576;
+		RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
+		long uptime = mxBean.getUptime() / 60000;
+		String formatString = "Ping: **%d** ms\nMemory: **%.2f / %d / %d** MiB\nUptime: **%d** minutes";
 		
-		command.getEvent().getChannel().sendMessage("...").queue(response -> {
-			response.editMessageFormat("Ping: **%d**ms", System.currentTimeMillis() - time).queue();
+		command.getEvent().getChannel().sendMessage("Pong!").queue(response -> {
+			response.editMessageFormat(formatString, System.currentTimeMillis() - time, mbUsed, mbTotal, mbMax, uptime).queue();
 		});
 		return (true);
 	}
